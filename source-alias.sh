8b02ffe514dba09e5e129ab09c5bdd1054127cdb
Makers <makers>
2026-02-01 22:14:02 +0800

    update source alias

--- source-alias.sh
+++ source-alias.sh
@@ -1,1 +1,3 @@
-echo >> "alias rebash='source ~/.bashrc'" >> ~/.bashrc
+grep -qxF "alias rebash='source ~/.bashrc'" ~/.bashrc || \
+echo "alias rebash='source ~/.bashrc'" >> ~/.bashrc
+source ~/.bashrc
